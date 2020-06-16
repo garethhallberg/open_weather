141399d3a5ede3878e498a55be55fa7eeaf36129
Gareth Hallberg <gareth@ghallberg.co.uk>
2020-06-16 08:36:41 +0100

    Initial Commit

---
+++ OpenWeather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OpenWeather
+//
+//  Created by gareth15 on 16/06/2020.
+//  Copyright © 2020 gareth15. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
