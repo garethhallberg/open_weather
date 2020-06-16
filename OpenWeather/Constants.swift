c127c4fc935fe52e66c727a73415a9ff1af89abd
Gareth Hallberg <gareth@ghallberg.co.uk>
2020-06-16 08:45:27 +0100

    Add API key

---
+++ OpenWeather/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  OpenWeather
+//
+//  Created by gareth15 on 16/06/2020.
+//  Copyright © 2020 gareth15. All rights reserved.
+//
+
+import Foundation
+
+
+struct K {
+    static let APIKey = "96f2d47a11bf0aa1eb6964cd4cc3a0bc"
+}
